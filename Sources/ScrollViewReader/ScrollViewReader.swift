f6e97878f445dac613828560612f63b198977278
Jorge Villalobos Beato <tiusender@gmail.com>
2022-05-07 22:16:10 -0400

    Initial commit

--- Sources/ScrollViewReader/ScrollViewReader.swift
+++ Sources/ScrollViewReader/ScrollViewReader.swift
@@ -1,6 +1,35 @@
-public struct ScrollViewReader {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct ViewOffsetKey: PreferenceKey {
+    public typealias Value = CGFloat
+    public static var defaultValue = CGFloat.zero
+    public static func reduce(value: inout Value, nextValue: () -> Value) {
+        value += nextValue()
+    }
+}
+
+public struct ScrollViewReader<Content:View> : View {
+    let content: () -> Content
+    let onScrollOffsetChange: (CGFloat) -> ()
+    
+    public init(onScrollOffsetChange: @escaping (CGFloat) -> (), @ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+        self.onScrollOffsetChange = onScrollOffsetChange
+    }
+
+    public var body: some View {
+        ScrollView {
+            content()
+            .background {
+                GeometryReader { proxy in
+                    let offset = proxy.frame(in: .named("scroll")).minY
+                    Color.clear.preference(key: ViewOffsetKey.self, value: offset)
+                }
+            }
+        }
+        .coordinateSpace(name: "scroll")
+        .onPreferenceChange(ViewOffsetKey.self) { value in
+            self.onScrollOffsetChange(value)
+        }
     }
 }
